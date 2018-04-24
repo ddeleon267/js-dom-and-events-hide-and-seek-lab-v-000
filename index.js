6f8e7ae020802b14aacda2baac3eec014f840dd0
github username <github email address>
2018-04-24 12:21:08 -0400

    Done.

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ function increaseRankBy(n) {
   }
 }
 
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
 // Define a function deepestChild() that pulls out the most deeply nested child from div#grand-node. (Remember,
 //   you can iterate over elements and call querySelector() and querySelectorAll() on them. This is challenging to
 //   implement correctly, but not beyond your ability!)
